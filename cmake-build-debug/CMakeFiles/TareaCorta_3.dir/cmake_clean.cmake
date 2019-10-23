fa51d6d7ba102e35dc20bb32f4234cacb8ff3399
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2019-10-22 23:27:10 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TareaCorta_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TareaCorta_3.dir/main.cpp.obj"
+  "TareaCorta_3.exe"
+  "TareaCorta_3.exe.manifest"
+  "TareaCorta_3.pdb"
+  "libTareaCorta_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaCorta_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
